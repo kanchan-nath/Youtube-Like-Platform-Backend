ed3a2cc83c57cb967b1c27eae3d8c891509ce010
Kanchan Nath <kanchannath.webdev@gmail.com>
2026-01-17 16:07:30 +0530

    minor changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,19 @@ import {ApiError} from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { User } from "../models/user.model.js";
 import {cloudinaryUpload} from "../utils/cloudinary.js"
+import jwt from "jsonwebtoken"
+
+const generateAccessAndRefreshTokens = asyncHandeler(async(userId) =>{
+    const user = await User.findById(userId)
+
+    const accessToken = user.generateAccessToken()
+    const refreshToken = user.generateRefreshToken()
+
+    user.refreshToken = refreshToken
+
+    user.save({ validateBeforeSave: false })
+    return { accessToken, refreshToken }
+})
 
 const registerUser = asyncHandeler(async (req, res) =>{
     const {userName, email, fullName, password, age} = req.body
@@ -40,6 +53,32 @@ const registerUser = asyncHandeler(async (req, res) =>{
     .status(200)
     .json(new ApiResponse(200, user, "Otp Send to email!"))
 
+})
+
+const logInUser = asyncHandeler(async(req,res) =>{
+    const {userName, email, password} = req.body
+
+    if(!userName && !email && !password){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const user = await User.findOne({
+        $or:[{userName}, {email}]
+    })
+
+
+})
+
+const forgetPassword = asyncHandeler(async(req, res) =>{
+    const {email} = req.body
+    
+    if(!email){
+        throw new ApiError(400, "Email is required")
+    }
+     
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Plzz check your email for change password"))
 })
 
 export {
